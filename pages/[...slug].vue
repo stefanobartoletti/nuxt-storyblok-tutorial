f4c1788a8caca0999bde47eac38aee31c5cfd3ce
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-11-04 16:01:11 +0000

    add dynamic pages

---
+++ pages/[...slug].vue
@@ -0,0 +1,13 @@
+
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
+
+<script setup>
+const { slug } = useRoute().params
+const story = await useAsyncStoryblok(
+  slug && slug.length > 0 ? slug.join('/') : 'home',
+  { version: 'draft' }
+)
+</script>
+
